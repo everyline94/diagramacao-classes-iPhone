e0a898f1033439bfcb0a6926f558d746066890f2
bruno larocca <brunolujanlarocca@gmail.com>
2023-10-02 10:54:49 -0300

    Desafio feito

---
+++ src/Iphone.java
@@ -0,0 +1,48 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+
+    @Override
+    public void ligar(String numero) {
+        //Implementação aqui
+    }
+
+    @Override
+    public void atender(String numero) {
+        //implementação aqui
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        //implementação aqui
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        //implementação aqui
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        //implementação aqui
+    }
+
+    @Override
+    public void atualizarPagina() {
+        //implementação aqui
+    }
+
+    @Override
+    public void tocar() {
+        //implementação aqui
+    }
+
+    @Override
+    public void pausar() {
+        //implementação aqui
+    }
+
+    @Override
+    public void selecionarMusica() {
+        //implementação aqui
+    }
+}
